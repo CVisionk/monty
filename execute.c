a2e49b4b152c8bfa950d05e3653dcde33ec42225
Collen <damd819@gmail.com>
2024-03-28 22:08:19 +0200

    bug04

--- execute.c
+++ execute.c
@@ -1,57 +1,115 @@
 #include "monty.h"
 
 /**
- * execute - execute commands
- * @stack: stack on nums;
- * @line_number: line in file
- * @opcode_input: input
+* execute - execute opcode
+* @stack: stack of linked list
+* @counter: line counter
+* @file: pointer to monty file stream
+* @content: line content
+*
+* Return: none
+*/
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+{
+	instruction_t opst[] = {
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop", f_pop},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
+
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
+	return (1);
+}
+#include "monty.h"
+
+/**
+ * f_pop - print top node on stack
+ * @head: pointer to pointer of first node
+ * @counter: line counter
+ *
+ * Return: none
  */
 
-void execute(stack_t **stack, unsigned int line_number, char opcode_input[])
+void f_pop(stack_t **head, unsigned int counter)
 {
-	size_t call_length;
-	size_t i = 0;
-	instruction_t call_functions[] = {
-		{"pop", pop_stack_queue},
-		{"pall", print_stack_queue},
-		{"pint", f_pint},
-		{"add", f_add}
+	stack_t *h;
 
-	};
-
-	call_length = sizeof(call_functions) / sizeof(call_functions[0]);
-
-	for (i = 0; i < call_length; i++)
+	if (*head == NULL)
 	{
-		if (strcmp(opcode_input, call_functions[i].opcode) == 0)
-		{
-			call_functions[i].f(stack, line_number);
-			return;
-		}
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
 
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode_input);
-	free_stack(stack, line_number);
-	fflush(stderr);
-
+	h = *head;
+	*head = h->next;
+	free(h);
 }
 
 /**
- * free_stack - frees the stack
- * @stack: head of stack
- * @line_number: lines read
+ * f_pint - print top node on stack
+ * @head: pointer to pointer of first node
+ * @counter: line counter
+ *
+ * Return: none
  */
 
-void free_stack(stack_t **stack, unsigned int line_number)
+void f_pint(stack_t **head, unsigned int counter)
 {
-	stack_t *head = *stack;
-	stack_t *temp;
-
-	(void)line_number;
-	while(head)
+	if (*head == NULL)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*head)->n);
+}
+#include "monty.h"
+
+/**
+* f_pall - print everything in stack
+* @head: pointer to pointer of first node
+* @counter: line counter (not used)
+*
+* Return: none
+*/
+
+void f_pall(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void)counter;
+
+	h = *head;
+	if (h == NULL)
+		return;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 }
--- main.c
+++ main.c
@@ -1,57 +1,52 @@
-#include <stdio.h>
-#include <string.h>
 #include "monty.h"
 
-/**
- * main - entry point
- * @argc: number of args
- * @argv: array of args
- * Return: 0
- */
+bus_t bus = {NULL, NULL, NULL, 0};
 
+/**
+* main - monty code interpreter
+* @argc: argument counter
+* @argv: argument vector
+*
+* Return: always 0
+*/
 int main(int argc, char *argv[])
 {
-	FILE *fp;
-	char *line;
+	char *content;
+	FILE *file;
 	size_t size = 0;
-	unsigned int line_number = 0;
-	const char delim[] = " \t\n";
-	char *token = NULL;
+	ssize_t read_line = 1;
 	stack_t *stack = NULL;
-	stack_t *tail = NULL;
+	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		fflush(stderr);
 		exit(EXIT_FAILURE);
 	}
 
-	fp = fopen(argv[1], "r");
-	if (!fp)
+	file = fopen(argv[1], "r");
+	bus.file = file;
+
+	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		fflush(stderr);
 		exit(EXIT_FAILURE);
 	}
 
-	while (getline(&line, &size, fp) > 0)
+	while (read_line > 0)
 	{
-		line_number++;
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
 
-		token = strtok(line, delim);
-		if (token != NULL && strcmp(token, "push") == 0)
-		{
-			token = strtok(NULL, delim);
-			add_stack(&stack, &tail, line_number, token);
-		} else if (token != NULL)
-		{
-			execute(&stack, line_number, token);
-		}
-		line = NULL;
+		if (read_line > 0)
+			execute(content, &stack, counter, file);
+
+		free(content);
 	}
-	fclose(fp);
-	free_stack(&stack, line_number);
 
+	free_stack(stack);
+	fclose(file);
 	return (0);
 }
